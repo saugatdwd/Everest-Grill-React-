0e56290879da1040a0fa39ca6635367387e5ddcf
sameerz99 <sameerlamaz99@gmail.com>
2024-04-01 12:11:08 +0545

    carousel

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import logo from '../../assets/images/logo.jpg'
+import bgimage1 from '../../assets/images/backgroundimage1.jpg'
+import  {Carousel}  from 'flowbite-react'
 function Home() {
   return (
     <>
   
-  <h1>This is for testing</h1>
     </>
   )
 }
