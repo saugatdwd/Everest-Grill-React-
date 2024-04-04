2bc00f7f650e07382d032dc14ba0c46da011389c
Saugat Dawadi <saugatdwd@gmail.com>
2024-04-04 13:31:02 +0545

    Background image is changed

--- src/Components/Menu/MenuSec.jsx
+++ src/Components/Menu/MenuSec.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import MenuCard from './MenuCard'
-import img from '../../assets/images/dish3.jpeg'
+import appetizer from '../../assets/images/appetizer.jpeg'
+import dessert from '../../assets/images/dessert.jpeg'
+import drinks from '../../assets/images/drinks.png'
+import breakfast from '../../assets/images/breakfast.png'
+import backgroundfood from '../../assets/images/backgroundfood.jpeg'
 import backgroundimg from '../../assets/images/menubackground.jpg'
 
 function MenuSec() {
@@ -11,24 +15,24 @@ function MenuSec() {
   }
   
   return (
-    <div style={cardStyle} className='h-auto md:h-[100vh] 
+    <div style={cardStyle} className='h-[100vh] md:h-[100vh] 
       '>
-      <div className='text-center text-3xl py-14'>Our Menu</div>
+      <div className='text-center text-white font-satisfy text-3xl py-14'>Our Menu</div>
       <div className='flex md:flex-row flex-col justify-center gap-[1.3rem] items-center md:items-start  md:py-0 h-[90vh] '>
       <MenuCard 
-      image={img}
+      image={breakfast}
       categoryName="Breakfast" 
       />
       <MenuCard 
-      image={img}
-      categoryName="Apetizer"
+      image={appetizer}
+      categoryName="Appetizer"
       />
       <MenuCard 
-      image={img}
+      image={drinks}
       categoryName="Drinks"
       />
       <MenuCard 
-      image={img}
+      image={dessert}
       categoryName="Dessert"
       />
     </div>
