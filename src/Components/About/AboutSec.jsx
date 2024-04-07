741946b4892d3158d66fa490804f2e282a30db10
Saugat Dawadi <saugatdwd@gmail.com>
2024-04-07 13:18:33 +0545

    Link to read more is given

--- src/Components/About/AboutSec.jsx
+++ src/Components/About/AboutSec.jsx
@@ -1,31 +1,56 @@
-import React from 'react'
-import aboutbanner from '../../assets/images/about-banner.jpg'
-import underline from '../../assets/images/separator.svg'
+import React from "react";
+import aboutbanner from "../../assets/images/about-banner.jpg";
+import underline from "../../assets/images/separator.svg";
+import {Link, NavLink } from "react-router-dom";
 function AboutSec() {
+
+  const handleClick = () => {
+    window.scrollTo(0,0)
+    
+  }
   return (
+
+    
     <>
-    <div className='bg-gray-200 h-[100vh] pt-14 md:pt-0 md:pb-0 pb-14'>
-    <div className="md:min-h-screen  max-w-[90vw] mx-auto flex flex-col-reverse md:flex-row items-center gap-y-5
-     ">
-    <div className='w-full md:w-2/4'>
-        <img src={aboutbanner} alt="about banner" />
-    </div>
-    <div className='w-full md:w-2/4 text-center space-y-6'>
-        <div className=''>
-        <p className='text-3xl'>Our Story</p>
-        <img src={underline} alt="..." className='w-[150px] mx-auto' />
+      <div className="bg-gray-200 h-[100vh] pt-14 md:pt-0 md:pb-0 pb-14">
+        <div
+          className="md:min-h-screen  max-w-[90vw] mx-auto flex flex-col-reverse md:flex-row items-center gap-y-5
+     "
+        >
+          <div className="w-full md:w-2/4">
+            <img src={aboutbanner} alt="about banner" />
+          </div>
+          <div className="w-full md:w-2/4 text-center space-y-6">
+            <div className="">
+              <p className="text-3xl">Our Story</p>
+              <img src={underline} alt="..." className="w-[150px] mx-auto" />
+            </div>
+            <h1 className=" text-6xl font-bold">Every Flavor tells a story</h1>
+            <p>
+              Nestled amidst the bustling streets of Kathmandu, Nepal, our
+              restaurant offers a culinary journey steeped in tradition and
+              innovation. With a cozy ambiance adorned with local artwork and
+              accents, guests are transported to a realm of warmth and
+              hospitality.
+            </p>
+
+            <div>
+              <Link
+              to="./aboutpage"
+              >
+              <button className="gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-3  border hover:scale-105 duration-200  hover:border-gray-800 hover:from-black hover:to-gray-900"
+               onClick={handleClick}
+               >
+                Read More
+              </button>          
+              </Link>
+            </div>
+           
+          </div>
         </div>
-        <h1 className=' text-6xl font-bold'>Every Flavor tells a story</h1>
-        <p>Nestled amidst the bustling streets of Kathmandu, Nepal, our restaurant offers a culinary journey steeped in tradition and innovation.
-             With a cozy ambiance adorned with local artwork and accents, guests are transported to a realm of warmth and hospitality.</p>
-             <button className="gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-3  border hover:scale-105 duration-200  hover:border-gray-800 hover:from-black hover:to-gray-900">
-  Read More
-</button>
-    </div>
-    </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default AboutSec
+export default AboutSec;
